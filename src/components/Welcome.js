cc9ba60efc13d0a4d9b5c704574c299fbbf0e09c
Rodrigo <rodrigolemosrl@hotmail.com>
2018-04-12 19:09:53 +0000

    Welcome message

---
+++ src/components/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class Welcome extends React.Component {
+    render() {
+        return (
+            <div className="col-xs-12 col-sm-6 col-md-6 col-sm-offset-3 col-md-offset-3" align="center">
+              <div className="gap">
+                <h1> Pick a note! </h1>
+              </div>
+            </div>
+        )
+    }
+}
