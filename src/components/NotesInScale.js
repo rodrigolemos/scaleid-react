ccf2424dea87b970b442cbe176acfc063463015a
Rodrigo <rodrigolemosrl@hotmail.com>
2018-04-12 18:38:22 +0000

    Triad identificator

--- src/components/NotesInScale.js
+++ src/components/NotesInScale.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
 export class NotesInScale extends React.Component {
-  render(){
+  render() {
     const notes = this.props.scale;
     const buttons = notes.map( (note, index) => {
+      
+      let btndesign = "btn btn-scale ";
+      if ( ( notes.length === 7 ) && ( [0, 2, 4].indexOf(index) >= 0 ) ) {
+        btndesign += "btn-danger";
+      } else {
+        btndesign += "btn-info";
+      }
+      
       return (
-        <button key={index} className="btn btn-scale btn-info">{note}</button>
+        <button key={index} className={ btndesign }>{note}</button>
       )
+      
     });
     return <div>{buttons}</div>
   }
